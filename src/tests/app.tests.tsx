58aae56e92160ba4f4dbc78a757aacbfed93b9cc
John Wieland <john.wieland.dev@gmail.com>
2024-06-22 10:21:41 +1000

    Added react testing library and encountered error with cjs loading of @canva/app-ui-kit. Warning is "Unexpected token 'export'" in avatar_default.file.svg

--- src/tests/app.tests.tsx
+++ src/tests/app.tests.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { App } from '../app';
+import { render } from '@testing-library/react';
+import '@testing-library/dom';
+
 
 describe('app', () => {
   it('fails', () => {
-    expect(true).toBe(false);
+    render(<App />);
   });
 });
