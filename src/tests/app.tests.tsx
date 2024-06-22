7191e14be096bfe13a057bfc4631763dcd57661b
John Wieland <john.wieland.dev@gmail.com>
2024-06-22 11:21:26 +1000

    Finally got a bare bones test working. Removed everything to do with canva. Needed to change some tsconfig.json options

--- src/tests/app.tests.tsx
+++ src/tests/app.tests.tsx
@@ -3,12 +3,14 @@
  */
 import React from 'react';
 import { App } from '../app';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/dom';
 
 
 describe('app', () => {
   it('fails', () => {
     render(<App />);
+
+    expect(screen.getByText("It works!"));
   });
 });
